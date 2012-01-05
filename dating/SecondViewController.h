36b78647e4588ad501c72223596f3c0f393963c1
Amit Apollo Barman <AmitApolloBarman@gmail.com>
2012-01-04 21:27:51 -0600

    Initial Commit

---
+++ dating/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  dating
+//
+//  Created by Amit Barman on 1/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
